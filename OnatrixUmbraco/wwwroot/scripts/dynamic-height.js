ec8210f43d1b0f1d8b96ee20e96eb23fc24ab271
EVettainen <emha91@icloud.com>
2024-09-07 23:47:31 +0200

    2024-09-07-23-47

---
+++ OnatrixUmbraco/wwwroot/scripts/dynamic-height.js
@@ -0,0 +1,9 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const backgroundDivs = document.querySelectorAll('.background-color');
+    backgroundDivs.forEach(function (backgroundDiv) {
+        let height = backgroundDiv.getAttribute('data-height');
+        if (height) {
+            backgroundDiv.style.setProperty('--dynamic-height', height + '%');
+        }
+    });
+});
